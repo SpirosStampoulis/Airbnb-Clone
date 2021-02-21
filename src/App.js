6a39bf4effcceee6fb894d4a0e5b6c4bc8c85292
Spiros Stampoulis <sstaboulis@gmail.com>
2021-02-21 18:29:43 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Footer from "./Footer";
+import Home from "./Home";
+import SearchPage from "./SearchPage";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
