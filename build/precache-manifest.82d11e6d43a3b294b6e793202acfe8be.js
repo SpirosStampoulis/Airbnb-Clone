fdcd070bb3a8641a182e33ce82bb8f8d445784c1
Spiros Stampoulis <sstaboulis@gmail.com>
2021-02-21 18:45:25 +0100

    build

---
+++ build/precache-manifest.82d11e6d43a3b294b6e793202acfe8be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5880cd994dd61ff5fbc5678e438da097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a07403f13520f20e23f",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "308bc2c96aa9fc5a79ee",
+    "url": "/static/css/main.abd737b6.chunk.css"
+  },
+  {
+    "revision": "9a07403f13520f20e23f",
+    "url": "/static/js/2.7440f58a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.7440f58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "308bc2c96aa9fc5a79ee",
+    "url": "/static/js/main.3f14d011.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
